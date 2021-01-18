69c79d4e21e7eda233e3d711e9d3352053bc713f
zhaoranz <zhaoran1124@gmail.com>
2021-01-18 15:41:20 +0100

    Update Identify hubs.R

--- Identify hubs.R
+++ Identify hubs.R
@@ -23,3 +23,38 @@ return(conn)
 
 
 hub_k <- lapply(mod_list, k, A=A) # A is the adjacency matrix, same dimension with the number of genes
+
+#Module membership:
+
+MEs <- moduleEigengenes(datE, modLabel)$eigengenes
+colnames(MEs) <- substring(colnames(MEs), 3)
+mods <- unique(modLabel) #all the modules we have
+mod_mem <- vector("list", length=length(mods)) 
+names(mod_mem) <- mods
+
+idxme <- match(names(mod_mem),colnames(MEs))
+mes <- MEs[,idxme]#OK
+
+for (i in 1: length(mod_mem)) {
+exprm <- datE[,mod_list[[i]]]
+mod_mem[[i]] <- ssort(bicor(exprm, mes[,i])[,1])
+}
+#calculation betweenness centraliy in weighted network
+library(tnet)
+bcw <- function(genes,adjMatrix,alphai){
+a1 <- adjMatrix[genes,genes]
+diag(a1) <- 0
+net <- as.tnet(a1, type="weighted one-mode tnet")
+
+# Get node "names" from the matrix
+nodeLabels <- colnames(a1)
+
+# Compute metrics
+b1 <- data.frame(betweenness_w(net,alpha=alphai))
+bw1 <- b1[,2]
+names(bw1) <- nodeLabels#same with genes
+return(bw1)
+}
+
+#weighted betweenness centrality
+wbc <- lapply(mod_list, function(x) bcw(x, A, 1))
